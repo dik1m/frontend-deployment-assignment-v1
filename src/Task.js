173aac7b32027923dc03cda79121c674eb2162f4
sodikin <frinderytb17@gmail.com>
2022-12-17 10:02:30 +0700

    finaallly

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://assigment-deploys-fe2549882.netlify.app/" // TODO: replace this
 const studentName = "SODIKIN" // TODO: replace this
 const studentId = "FE2549882" // TODO: replace this
 
